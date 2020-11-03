a53bbdb5a56dfe06522c764edd47f6d05d195eff
Joachim Meyer <j.meyer@utah.edu>
2020-11-03 10:49:02 -0700

    Prototypes - Add sample map data display.
    Add prototype for displaying the map using Leaflet-JS and the GeoRaster
    layer lib.

---
+++ prototypes/topo/uac_class.js
@@ -0,0 +1,68 @@
+class AvalancheLevel {
+    static get low() { return '#00c346' }
+    static get moderate() { return '#fcfc00' }
+    static get considerate() { return '#ff9100' }
+    static get high() { return '#cc332c' }
+    static get extreme() { return '#222222' }
+}
+
+class MapData {
+    static get uacClasses() {
+        return 'LCC_uac_30m_classified.tif'
+    }
+
+    static get zoomLevel() { return 12; }
+    static get centerCoords() { return [40.60, -111.67]; }
+
+    static get attribution() {
+        return '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors';
+    }
+}
+
+class Map {
+    constructor() {
+        this.baseLayer = L.map("map-area").setView(
+            MapData.centerCoords, MapData.zoomLevel
+        );
+        L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png',
+            {
+                attribution: MapData.attribution
+            }
+        ).addTo(this.baseLayer);
+    }
+
+    classToColor(value) {
+        if( value[0] <= 8 ) {
+            return AvalancheLevel.low;
+        } else if ( value[0] <= 16 ) {
+            return AvalancheLevel.moderate;
+        } else {
+            return AvalancheLevel.considerate;
+        }
+    }
+
+    addLayer(layerData) {
+        /**
+         * https://github.com/GeoTIFF/georaster-layer-for-leaflet
+         */
+        this.Dangerlayer = new window.GeoRasterLayer({
+            georaster: layerData,
+            opacity: 0.4,
+            pixelValuesToColorFn: values => {
+                return this.classToColor(values)
+            },
+            debugLevel: 0,
+        });
+
+        this.Dangerlayer.addTo(this.baseLayer);
+        // this.baseLayer.fitBounds(this.Dangerlayer.getBounds());
+    }
+
+    load() {
+        let that = this;
+        fetch(MapData.uacClasses)
+            .then(response => response.arrayBuffer())
+            .then(window.parseGeoraster)
+            .then(goeRaster => that.addLayer(goeRaster));
+    }
+}
