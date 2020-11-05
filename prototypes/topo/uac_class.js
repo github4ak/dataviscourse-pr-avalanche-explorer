ba5d03e0ce40d64fa9fecb45cc9c8420f53ffa68
Joachim Meyer <j.meyer@utah.edu>
2020-11-05 11:44:57 -0700

    Prototype - Map - Add popup with terrain info.
    Adding a pop with the terrain info once a user selects a point on the
    map via click.

--- prototypes/topo/uac_class.js
+++ prototypes/topo/uac_class.js
@@ -6,20 +6,20 @@ class AvalancheDangerColor {
     static get extreme() { return '#222222' }
 
     static get map() {
-        return [
-            null,
-            AvalancheDangerColor.low,
-            AvalancheDangerColor.moderate,
-            AvalancheDangerColor.considerate,
-            AvalancheDangerColor.high,
-            AvalancheDangerColor.extreme,
-        ]
+        return {
+           'None':  null,
+           'Low':  AvalancheDangerColor.low,
+           'Moderate':  AvalancheDangerColor.moderate,
+           'Considerate':  AvalancheDangerColor.considerate,
+           'High':  AvalancheDangerColor.high,
+           'Extreme':  AvalancheDangerColor.extreme,
+        }
     }
 }
 
 class MapData {
     static get uacClasses() {
-        return 'LCC_uac_30m_classified.tif'
+        return 'LCC_uac_30m_4326.tif'
     }
 
     static get zoomLevel() { return 12; }
@@ -31,26 +31,53 @@ class MapData {
 }
 
 class Map {
+    get currentMarker() { return this._currentMarker; }
+    set currentMarker(marker) { this._currentMarker = marker; }
+
+    removeMarker() {
+        if (this._currentMarker) this.currentMarker.remove();
+    }
+
+    moveMarker(e) {
+        this.removeMarker();
+        this.currentMarker = L.marker(e.latlng);
+        this.currentMarker.addTo(this.baseLayer);
+        const popup = this.currentMarker.bindPopup(
+            this.infoAtLatLng(e.latlng.lat, e.latlng.lng)
+        ).openPopup();
+    }
+
+    get uacClassInfo() {
+       return this.raster.values[0];
+    }
+    get slopeInfo() {
+        return this.raster.values[1];
+    }
+
     constructor() {
         this.baseLayer = L.map("map-area").setView(
             MapData.centerCoords, MapData.zoomLevel
         );
-        // L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png',
         L.tileLayer('https://a.tile.opentopomap.org/{z}/{x}/{y}.png',
             {
                 attribution: MapData.attribution,
                 maxZoom: 17,
             }
         ).addTo(this.baseLayer);
+
+        const that = this;
+        this.baseLayer.on('click', function() {
+            that.removeMarker();
+        })
     }
 
     classToColor(value) {
         if( value[0] <= 8 ) {
-            return AvalancheDangerColor.map[1];
+            return AvalancheDangerColor.map.Low;
         } else if ( value[0] <= 16 ) {
-            return AvalancheDangerColor.map[2];
+            return AvalancheDangerColor.map.Moderate;
         } else {
-            return AvalancheDangerColor.map[3];
+            return AvalancheDangerColor.map.Considerate;
         }
     }
 
@@ -66,9 +93,61 @@ class Map {
             },
             debugLevel: 0,
         });
+        this.raster = this.dangerlayer.georasters[0];
 
         this.dangerlayer.addTo(this.baseLayer);
-        // this.baseLayer.fitBounds(this.Dangerlayer.getBounds());
+
+        const that = this;
+        this.uacInfo = L.imageOverlay(
+            MapData.uacClasses,
+            [
+                [this.dangerlayer.ymin, this.dangerlayer.xmin],
+                [this.dangerlayer.ymax, this.dangerlayer.xmax]
+            ],
+            {
+                interactive: true,
+                opacity: 0,
+            }
+        ).on('click', function (e) {
+            that.moveMarker(e);
+            L.DomEvent.stopPropagation(e);
+        });
+
+        this.uacInfo.addTo(this.baseLayer);
+    }
+
+    redraw() {
+        this.dangerlayer.redraw();
+    }
+
+    lngToRasterX(lng) {
+        return Math.floor(
+            this.raster.width *
+            Math.abs(lng - this.raster.xmin) /
+            (this.raster.xmax - this.raster.xmin)
+        );
+    }
+
+    latToRasterY(lat) {
+        return this.raster.height - Math.ceil(
+            this.raster.height *
+            Math.abs(lat - this.raster.ymin) /
+            (this.raster.ymax - this.raster.ymin)
+        )
+    }
+
+     infoAtLatLng (lat, lng) {
+        try {
+            const x = this.lngToRasterX(lng);
+            const y = this.latToRasterY(lat);
+            const uacID = this.uacClassInfo[y][x];
+            return `UAC class: ${UACMapper.id[uacID].Elevation}</br>` +
+                `Aspect: ${UACMapper.id[uacID].Aspect}</br>` +
+                `Slope Angle: ${this.slopeInfo[y][x]}`;
+        }
+        catch(err) {
+            return 'No value';
+        }
     }
 
     load() {
---
+++ prototypes/topo/uac_mapper.js
@@ -0,0 +1,114 @@
+class UACMapper {
+    static get lowElevation() {
+        return 'Low Elevation'
+    }
+
+    static get midElevation() {
+        return 'Mid Elevation'
+    }
+
+    static get highElevation() {
+        return 'High Elevation'
+    }
+
+    static get id() {
+        return {
+            1: {
+                'Aspect': 'North',
+                'Elevation': UACMapper.lowElevation
+            },
+            2: {
+                'Aspect': 'North',
+                'Elevation': UACMapper.lowElevation
+            },
+            3: {
+                'Aspect': 'North',
+                'Elevation': UACMapper.lowElevation
+            },
+            4: {
+                'Aspect': 'North',
+                'Elevation': UACMapper.lowElevation
+            },
+            5: {
+                'Aspect': 'North',
+                'Elevation': UACMapper.lowElevation
+            },
+            6: {
+                'Aspect': 'North',
+                'Elevation': UACMapper.lowElevation
+            },
+            7: {
+                'Aspect': 'North',
+                'Elevation': UACMapper.lowElevation
+            },
+            8: {
+                'Aspect': 'North',
+                'Elevation': UACMapper.lowElevation
+            },
+            9: {
+                'Aspect': 'North',
+                'Elevation': UACMapper.midElevation
+            },
+            10: {
+                'Aspect': 'North',
+                'Elevation': UACMapper.midElevation
+            },
+            11: {
+                'Aspect': 'North',
+                'Elevation': UACMapper.midElevation
+            },
+            12: {
+                'Aspect': 'North',
+                'Elevation': UACMapper.midElevation
+            },
+            13: {
+                'Aspect': 'North',
+                'Elevation': UACMapper.midElevation
+            },
+            14: {
+                'Aspect': 'North',
+                'Elevation': UACMapper.midElevation
+            },
+            15: {
+                'Aspect': 'North',
+                'Elevation': UACMapper.midElevation
+            },
+            16: {
+                'Aspect': 'North',
+                'Elevation': UACMapper.midElevation
+            },
+            17: {
+                'Aspect': 'North',
+                'Elevation': UACMapper.highElevation
+            },
+            18: {
+                'Aspect': 'North',
+                'Elevation': UACMapper.highElevation
+            },
+            19: {
+                'Aspect': 'North',
+                'Elevation': UACMapper.highElevation
+            },
+            20: {
+                'Aspect': 'North',
+                'Elevation': UACMapper.highElevation
+            },
+            21: {
+                'Aspect': 'North',
+                'Elevation': UACMapper.highElevation
+            },
+            22: {
+                'Aspect': 'North',
+                'Elevation': UACMapper.highElevation
+            },
+            23: {
+                'Aspect': 'North',
+                'Elevation': UACMapper.highElevation
+            },
+            24: {
+                'Aspect': 'North',
+                'Elevation': UACMapper.highElevation
+            },
+        }
+    }
+}
