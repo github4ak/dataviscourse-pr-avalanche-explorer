b483a5feb9d853b0fa05ea0210a71148952d413a
Joachim Meyer <j.meyer@utah.edu>
2020-11-22 11:08:08 -0700

    RoseMenu - Wire up the dropdown boxes.

--- js/uac_rose_menu.js
+++ js/uac_rose_menu.js
@@ -4,19 +4,45 @@ class RoseMenu {
     }
 
     addOptions() {
-        d3.select('#rose-aspects')
-            .selectAll("option")
+        let that = this;
+        this.aspects = d3.select('#rose-aspects');
+        this.aspects.selectAll("option")
             .data(UACMapper.ASPECTS.keys())
             .enter()
             .append("option")
             .text(d => d)
             .attr("value", d => d);
-        d3.select("#rose-elevations")
-            .selectAll("option")
+
+        this.aspects
+            .on('change', function (e) {
+                that.elevations.node().selectedIndex = 0;
+                that.updateMap(this, UACMapper.ASPECTS);
+            });
+
+        this.elevations = d3.select("#rose-elevations");
+        this.elevations.selectAll("option")
             .data(UACMapper.ELEVATIONS.keys())
             .enter()
             .append("option")
             .text((d) => d)
             .attr("value", (d) => d);
+
+        this.elevations
+            .on('change', function () {
+                that.aspects.node().selectedIndex = 0;
+                that.updateMap(this, UACMapper.ELEVATIONS);
+            });
+    }
+
+    clear() {
+        this.aspects.node().selectedIndex = 0;
+        this.elevations.node().selectedIndex = 0;
+    }
+
+    updateMap(menu, data) {
+        this.rose.map.selection = data.get(
+            menu.options[menu.selectedIndex].value
+        ).map(a => a.ID);
+        this.rose.map.redraw();
     }
 }
